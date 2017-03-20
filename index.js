b72b72bbffa928a0cb5f898741af4e592a3c42ee
spasiu <stasi@smooch.io>
2017-03-20 08:02:24 -0400

    replace html with EJS to get client id into button

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 require('dotenv').config();
 
+const fs = require('fs');
 const https = require('https');
 const qs = require('querystring');
 const express = require('express');
 const bodyParser = require('body-parser');
 const events = require('./events');
+const ejs = require('ejs');
+const authEjs = fs.readFileSync(__dirname + '/auth-template.ejs.html', 'utf-8');
+const authHtml = ejs.render(authEjs, { clientId: process.env.CLIENT_ID });
 
 const port = process.env.PORT || 8000;
 
 express()
     .use(bodyParser.json())
-    .get('/auth', (req, res) => res.sendFile(__dirname + '/public/auth.html'))
+    .get('/auth', (req, res) => res.send(authHtml))
     .get('/redirect', (req, res) => {
         https.get('https://slack.com/api/oauth.access?' + qs.stringify({
             client_id: process.env.CLIENT_ID,
